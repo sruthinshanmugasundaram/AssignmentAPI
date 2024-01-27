06d566e7283d46d83865947ef9e839d5df1b2040
sruthinshanmugasundaram <113575382+sruthinshanmugasundaram@users.noreply.github.com>
2024-01-27 09:06:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,19 @@ mongoose
     const allowedOrigins = ['https://dynamic-pastelito-a75099.netlify.app'];
 
 app.use(
-  cors()
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+  })
 );
 
     // Routes
